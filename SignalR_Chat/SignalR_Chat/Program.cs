cdf1ac4010fcd2c04b5b50815cacb643c7e74f4e
Muhammad Haseeb <shahzadahmad9353680@gmail.com>
2023-07-13 16:52:37 +0500

    First Commit of SignalR

---
+++ SignalR_Chat/SignalR_Chat/Program.cs
@@ -0,0 +1,36 @@
+using SignalR_Chat.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Including the Service of SignalR
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// End Point
+app.MapHub<ChatHub>("/ChatHub");
+
+app.Run();
